fac7c70d4c43e05265a637b652668a394b62686f
+ <+>
2021-09-30 20:30:52 -0700

    dynamic components lab

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -2,18 +2,29 @@ import React, { Component } from 'react';
 
 export default class ColorBox extends Component {
 
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {opacity: this.props.opacity}
+  //   console.log(this.state.opacity)
+  // }
+
   state = {
     todos: [
       
     ]
   }
 
-  render() {
-    return (
-      <div className="color-box" style={{opacity: null /*replace null with the value*/}}>
-        {/* your conditional code here! */}
-      </div>
-    )
+  render (props) {
+    if (this.props.opacity >= 0.2) {
+      console.log(this.props.opacity)
+      return (
+        <div className="color-box" style={{opacity: this.props.opacity}}>
+          <ColorBox opacity={this.props.opacity - 0.1} />
+        </div>
+      )
+    } else {
+      return null
+    }
   }
 
 }
